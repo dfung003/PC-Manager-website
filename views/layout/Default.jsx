0af90d99cab0215bd1054a993d56087120bdd96c
dfung003 <dfung003@ucr.edu>
2022-03-06 17:25:02 -0800

    imported bootstrap stylesheet, added header and footer to Default.jsx

--- views/layout/Default.jsx
+++ views/layout/Default.jsx
@@ -6,13 +6,15 @@ class DefaultLayout extends React.Component {
             <html>
                 <head>
                     <title>{this.props.title}</title>
+                    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossOrigin="anonymous" />
                     <link rel="stylesheet" href="/css/app.css" /> 
                 </head>
-                <div>
                     <body>
+                        <header>PCManager</header>
                         {this.props.children}
+                        <footer>Created By: Damon Fung</footer>
+
                     </body>
-                </div>
             </html>
         )
     }
