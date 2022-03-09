038bf0a20085ad20ada7dfddc9ac5b86b435d0b1
dfung003 <dfung003@ucr.edu>
2022-03-08 18:56:06 -0800

    changing mongoose.Schema to new Schema

--- models/reviews.js
+++ models/reviews.js
@@ -1,6 +1,6 @@
 const { Schema, model } = require('mongoose');
 
-const reviewsSchema = mongoose.Schema({
+const reviewsSchema = new Schema({
     author: String,
     productName: String,
     rating: { type: Number, enum: [1,2,3,4,5], default: 5 }
