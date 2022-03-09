4f0ea4b5be58943cd2ee80af65e763d10967172f
dfung003 <dfung003@ucr.edu>
2022-03-08 19:16:17 -0800

    Creating Index.jsx for product reviews

---
+++ views/reviews/Index.jsx
@@ -0,0 +1,33 @@
+const React = require('react');
+const Default = require('../layout/Default.jsx')
+
+class Index extends React.Component {
+    render(){
+        const { reviews } = this.props;
+        return (
+            <Default>
+                <div id="reviews-title">
+                    <h1>Product Reviews</h1>
+                    <nav>
+                        <a href="/reviews/new">Add A New Product Review</a>
+                    </nav>
+                    <br />
+                    {
+                        reviews.map((review) => (
+                            <article className="review">
+                                <h1>Product Reviews</h1>
+                                <a href={`/reviews/${review._id}`}>
+                                    <h2>
+                                        {review.author} - {review.productName}
+                                    </h2>
+                                </a>
+                            </article>
+                        ))
+                    }
+                </div>
+            </Default>
+        )
+    }
+}
+
+module.exports = Index;
